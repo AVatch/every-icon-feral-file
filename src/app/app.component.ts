e87dc01eb15e5319a83b4c065848faeddc871fc8
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-04-01 09:47:33 -0400

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'every-icon-feral-file';
+
+  constructor() {
+    const app = initializeApp(environment.firebase);
+  }
 }
